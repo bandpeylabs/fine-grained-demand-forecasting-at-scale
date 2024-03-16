764d40dce983df93acdf0e9b6494eeaf8c84fffc
r.bandpey67@gmail.com <r.bandpey67@gmail.com>
2024-03-16 15:51:09 +0000

    added image

--- src/01-data-prep.py
+++ src/01-data-prep.py
@@ -1,2 +1,20 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC #Data Preparation and EDA
+# MAGIC  
+# MAGIC In this notebook, we'll load our data into Databricks, create our training and test sets, and explore our data.
+# MAGIC
+# MAGIC ## Details
+# MAGIC
+# MAGIC - Load data into the workspace using the UI
+# MAGIC - Save transformed data as Delta Lake tables
+# MAGIC - Use Python to parameterize Spark SQL queries
+# MAGIC - Create visualizations with Databricks native plotting tools
+# MAGIC - Use widgets to create interactive plots for dashboarding
 
+# COMMAND ----------
+
+
+
+
+data-upload.png
