63bf47a81134fdb05fecb08e24b2cd0278244008
r.bandpey67@gmail.com <r.bandpey67@gmail.com>
2024-03-17 15:44:13 +0000

    EDA compeleted

--- src/01-data-prep.py
+++ src/01-data-prep.py
@@ -173,8 +173,141 @@ saveDeltaTable(testDF, path_test, tableName_test)
 
 # COMMAND ----------
 
-dbutils.fs.ls(path_train)
+# MAGIC %md
+# MAGIC ## Examine the Data
+# MAGIC We'll use our training data during EDA; as such, our trend analyses will be missing the month of December 2017.
+# MAGIC
+# MAGIC Note that the results of SQL queries in Databricks notebooks are equivalent to the results of a `display()` function on a DataFrame. We'll use to visualize our trends.
+# MAGIC
+# MAGIC When performing demand forecasting, we are often interested in general trends and seasonality. Let's start our exploration by examing the annual trend in unit sales:
 
 # COMMAND ----------
 
+# MAGIC %sql
+# MAGIC
+# MAGIC SELECT
+# MAGIC   year(date) as year, 
+# MAGIC   sum(sales) as sales
+# MAGIC FROM train
+# MAGIC GROUP BY year(date)
+# MAGIC ORDER BY year;
 
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC It's very clear from the data that there is a generally upward trend in total unit sales across the stores. If we had better knowledge of the markets served by these stores, we might wish to identify whether there is a maximum growth capacity we'd expect to approach over the life of our forecast.  But without that knowledge and by just quickly eyeballing this dataset, it feels safe to assume that if our goal is to make a forecast a few days, weeks or months from our last observation, we might expect continued linear growth over that time span.
+# MAGIC
+# MAGIC Now let's examine seasonality.  If we aggregate the data around the individual months in each year, a distinct yearly seasonal pattern is observed which seems to grow in scale with overall growth in sales:
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC SELECT 
+# MAGIC   TRUNC(date, 'MM') as month,
+# MAGIC   SUM(sales) as sales
+# MAGIC FROM train
+# MAGIC GROUP BY TRUNC(date, 'MM')
+# MAGIC ORDER BY month;
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Aggregating the data at a weekday level, a pronounced weekly seasonal pattern is observed with a peak on Sunday (weekday 1), a hard drop on Monday (weekday 2) and then a steady pickup over the week heading back to the Sunday high. This pattern seems to be pretty stable across the five years of observations, increasing with scale given the overall annual trend in sales growth:
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC SELECT
+# MAGIC   YEAR(date) as year,
+# MAGIC   DAYOFWEEK(date) % 7 as weekday,
+# MAGIC   AVG(sales) as sales
+# MAGIC FROM (
+# MAGIC   SELECT 
+# MAGIC     date,
+# MAGIC     SUM(sales) as sales
+# MAGIC   FROM train
+# MAGIC   GROUP BY date
+# MAGIC  ) x
+# MAGIC GROUP BY year, DAYOFWEEK(date)
+# MAGIC ORDER BY year, weekday;
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC But as mentioned at the top of this section, there are 10 stores represented in this dataset, and while these stores seem to behave in similar manners, there is still some variation between them as can be seen with a close examination of average monthly sales between stores:
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC SELECT
+# MAGIC   MONTH(month) as month,
+# MAGIC   store,
+# MAGIC   AVG(sales) as sales
+# MAGIC FROM (
+# MAGIC   SELECT 
+# MAGIC     TRUNC(date, 'MM') as month,
+# MAGIC     store,
+# MAGIC     SUM(sales) as sales
+# MAGIC   FROM train
+# MAGIC   GROUP BY TRUNC(date, 'MM'), store
+# MAGIC   ) x
+# MAGIC GROUP BY MONTH(month), store
+# MAGIC ORDER BY month, store;
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Parameterize a SQL Query
+# MAGIC
+# MAGIC Below, a Python method that returns monthly sales for a single item, grouped by store. The function accepts a single argument for item `sku`.
+
+# COMMAND ----------
+
+def monthlyItemSales(sku:int):
+  display(spark.sql(f"""
+    SELECT
+      MONTH(month) as month,
+      store,
+      AVG(sales) as sales
+    FROM (
+      SELECT 
+        TRUNC(date, 'MM') as month,
+        store,
+        SUM(sales) as sales
+      FROM train
+      WHERE item={sku}
+      GROUP BY TRUNC(date, 'MM'), store
+      ) x
+    GROUP BY MONTH(month), store
+    ORDER BY month, store;
+   """))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC The cell below defined a dropdown widget that includes all 50 of our item SKUs.
+
+# COMMAND ----------
+
+dbutils.widgets.dropdown("sku", "1", [str(x) for x in range(1,51)])
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Use the function to visually explore trends of several items. Make a line chart with the following specifications:
+# MAGIC - keys: `month`
+# MAGIC - series groupings: `store`
+# MAGIC - values: `sales`
+
+# COMMAND ----------
+
+monthlyItemSales(dbutils.widgets.get("sku"))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC This data exploration reveals the fundamental issue we wish to tackle in this demonstration.  When we examine our data at an aggregate level, i.e. summing sales across stores or individual products, the aggregation hides what could be some useful variability in our data.  By analyzing our data at the store-item level, we hope to be able to construct forecasts capable of picking up on these subtle differences in behaviours.
+# MAGIC
