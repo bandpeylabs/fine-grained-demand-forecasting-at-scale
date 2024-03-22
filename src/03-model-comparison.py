3c6401980856d6b685e5a88fe4e867f1236b6111
r.bandpey67@gmail.com <r.bandpey67@gmail.com>
2024-03-22 23:00:56 +0000

    Plot results

--- src/03-model-comparison.py
+++ src/03-model-comparison.py
@@ -117,4 +117,118 @@ display(item_accum_spark.head(5))
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ## Scale Allocation Predictions by Historical Sales Figures
+# MAGIC
+# MAGIC Now let's calculate the ratio of store-specific sales to total sales for each product.  This will give us the ratios that we need for allocation.
+
+# COMMAND ----------
+
+from pyspark.sql.window import Window
+
+# calculate per-item store sales ratios
+item_store_ratios = (
+  trainDF
+    .groupBy('store','item')
+    .agg(F.sum('sales').alias('store_item_sales'))
+    .withColumn('item_sales', F.sum('store_item_sales').over(Window.partitionBy('item')))
+    .withColumn('ratio', F.expr('store_item_sales/item_sales'))
+    .select('item', 'store', 'ratio', 'store_item_sales', 'item_sales')
+    .orderBy(['item', 'store'])
+  )
+
+display(item_store_ratios)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Applying these ratios to our cross-store aggregates, we can now produce an allocated forecast for each store-item combination.
+# MAGIC
+# MAGIC In the cell below, we join our `item_store_ratios` with the predictions we made above. Save out a Delta Lake table `agg_item_preds` with the following schema:
+# MAGIC
+# MAGIC | field | type |
+# MAGIC | --- | --- |
+# MAGIC | store | INT |
+# MAGIC | item | INT |
+# MAGIC | ds | DATE |
+# MAGIC | yhat | FLOAT |
+
+# COMMAND ----------
+
+tableName = "agg_item_preds"
+tablePath = "/mnt/demand_forecasting/agg_item_preds"
+
+allocatedDF = (item_accum_spark
+  .join(item_store_ratios, ["item"], "outer")
+  .select("store", "item", "ds", (F.col("yhat") * F.col("ratio")).alias("yhat")))
+
+(allocatedDF.write
+  .format("delta")
+  .option("path", tablePath)
+  .mode("overwrite")
+  .saveAsTable(tableName)
+)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Calculate Difference in Model Performance
+# MAGIC
+# MAGIC The query below will join our `test`, `store_item_preds`, and `agg_item_preds` tables to provide a quick overview of how our predictions vary from each other and the true sales.
+# MAGIC
+# MAGIC | field | description |
+# MAGIC | --- | --- |
+# MAGIC | y | true sales |
+# MAGIC | yhat | store-item predicted sales |
+# MAGIC | yhat_lower | lower bound of 95% confidence interval for store-item predicted sales |
+# MAGIC | yhat_upper | upper bound of 95% confidence interval for store-item predicted sales |
+# MAGIC | yhat_agg | predicted sales from ratio-allocation on item-aggregated sales |
+# MAGIC | pred_diff | the difference between `yhat` and `yhat_agg` |
+# MAGIC | miss | difference between true sales and `yhat` |
+# MAGIC | agg_miss | difference between true sales and `yhat_agg`
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE OR REPLACE TEMP VIEW pred_overview AS (
+# MAGIC   SELECT a.date,
+# MAGIC     a.store,
+# MAGIC     a.item,
+# MAGIC     a.sales AS y,
+# MAGIC     b.yhat,
+# MAGIC     b.yhat_lower,
+# MAGIC     b.yhat_upper,
+# MAGIC     c.yhat AS yhat_agg, 
+# MAGIC     b.yhat - c.yhat AS pred_diff, 
+# MAGIC     a.sales - b.yhat AS miss, 
+# MAGIC     a.sales - c.yhat AS agg_miss
+# MAGIC   FROM test a
+# MAGIC   INNER JOIN store_item_preds b
+# MAGIC   ON a.date = b.ds AND a.store=b.store AND a.item=b.item
+# MAGIC   INNER JOIN agg_item_preds c
+# MAGIC   ON a.date = c.ds AND a.store=c.store AND a.item=c.item
+# MAGIC )
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC But what do these differences really amount to?  How much higher and how much lower are my forecasts when compared to one another? If we just compare our forecasts for the 30-day future period over which we are estimating demand, the differences add up differently by product and by store which leads us to the conclusion that the *goodness* or *badness* of one strategy over the other really depends on the stores and products we are considering, but in some instances there are some sizeable misses that can have implications for out-of-stock and overstock scenarios.
+# MAGIC
+# MAGIC Let's look at the results for a single item.
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * 
+# MAGIC FROM pred_overview
+# MAGIC WHERE item = 1
+# MAGIC ORDER BY date, store
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Note that if we use a line plot to compare our `miss` and `agg_miss` variables, we can visualize that both models pick up on very similar trends.
+
+# COMMAND ----------
+
 
