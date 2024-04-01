c1b97ce26ce45acf79cb899764097c160830ad2a
r.bandpey67@gmail.com <r.bandpey67@gmail.com>
2024-04-01 13:12:46 +0000

    Fetch data

--- src/05-demo.py
+++ src/05-demo.py
@@ -1,2 +1,18 @@
 # Databricks notebook source
+# MAGIC %run "./config/setup"
+
+# COMMAND ----------
+
+trainDF = spark.table("train")
+
+# COMMAND ----------
+
+display(trainDF)
+
+# COMMAND ----------
+
+# MAGIC %md When performing demand forecasting, we are often interested in general trends and seasonality. Let's start our exploration by examining the annual trend in unit sales:
+
+# COMMAND ----------
+
 
